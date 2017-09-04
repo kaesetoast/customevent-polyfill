9a41479b165aa055904068100dd96386562a67f7
Philipp Nowinski <philipp@sgalinski.de>
2017-09-04 17:24:04 +0200

    [FEATURE] add UMD header

--- customevent-polyfill.js
+++ customevent-polyfill.js
@@ -1,4 +1,8 @@
-(function () {
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) define(factory);
+    else if (typeof exports === 'object') module.exports = factory();
+    else root.__customEventPolyfill = factory();
+}(this, function() {
     'use strict';
     function CustomEvent (event, params) {
         params = params || { bubbles: false, cancelable: false, detail: undefined };
@@ -22,4 +26,4 @@
     }
 
     window.CustomEvent = CustomEvent;
-})();
+}));
